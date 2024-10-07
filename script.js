c30eb159eb51cba77ceac1d4795b7128308dbc98
roman15763 <rodanvashchuk@gmail.com>
2024-10-07 18:29:11 +0300

    Update script.js

--- script.js
+++ script.js
@@ -8,11 +8,28 @@ document.getElementById('loginForm').addEventListener('submit', function (event)
     const password = document.getElementById('password').value;
 
     // Перевірка користувача
-    if (firstName === "рома" && lastName === "ващук" && group === "1-кт-25" && password === "ваш_пароль") {
-        window.location.href = "page1.html"; // Сторінка для Роми
-    } else if (firstName === "слава" && lastName === "ващук" && group === "1-кт-26" && password === "2505") {
-        window.location.href = "page2.html"; // Сторінка для Слави
+  if (
+      firstName === "рома" &&
+      lastName === "ващук" &&
+      group === "1-кт-25" &&
+      password === "ваш_пароль"
+    ) {
+      window.location.href = "page1.html"; // Сторінка для Роми
+    } else if (
+      firstName === "слава" &&
+      lastName === "ващук" &&
+      group === "1-кт-26" &&
+      password === "2505"
+    ) {
+      window.location.href = "page2.html"; // Сторінка для Слави
+    } else if (
+      firstName === "йй" &&
+      lastName === "йй" &&
+      group === "йй" &&
+      password === "йй"
+    ) {
+      window.location.href = "page2.html"; // Сторінка для Слави
     } else {
-        document.getElementById('error').innerText = "Невірні дані входу!";
+      document.getElementById("error").innerText = "Невірні дані входу!";
     }
 });
